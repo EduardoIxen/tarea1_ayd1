35613abcb30e001e8b2d814057ade19c6d81ac9e
EduardoIxen <tomas-1539@hotmail.com>
2023-02-18 17:58:30 -0600

    [ADD] Endpoint para sumar dos numeros
    - Este endpoint recibe un json que contiene dos numeros, los suma y retorna el resultado.
    - Verifica que si lo que recibe no es un json retorna un mensaje de error.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
+@app.post('/calculate')
+def calculate():
+    data = request.json
+    if request.is_json:
+        num1 = data['num1']
+        num2 = data['num2']
+        result = int(num1) + int(num2)
+        return {'result': result}, 200
+
+    return {'error': 'Invalid input'}, 400
 
 if __name__ == '__main__':
     app.run()
